f83bf10cf57b2532c399b1634a5eb08e990f0c75
Jason Duong <jasontduong@outlook.com>
2022-08-03 03:08:59 -0700

    updated UI

--- calculator.js
+++ calculator.js
@@ -1,5 +1,9 @@
+const display = document.querySelector('.calcdisplay');
+const buttons = document.getElementsByClassName("buts");
+
+
 function add(a, b) {
-  return a + b;
+    return a + b;
 }
 
 function subtract(a, b) {
@@ -11,7 +15,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        return "BRUH"
+    } else {
     return a / b;
+    }
 }
 
 function operate(a, b, operator) {
@@ -21,7 +29,23 @@ function operate(a, b, operator) {
     if (operator === divide) return divide(a, b);
 }
 
+function buttonClicks() {
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function (event) {
+             console.log(event.target.value)
+             
+         });
+    }
+}
+
+
+
+
 console.log(operate(3, 4, add));
 console.log(operate(3, 4, subtract));
 console.log(operate(3, 4, multiply));
 console.log(operate(16, 4, divide));
+
+window.onload = () => {
+    displayThis();
+}
